71d51460ab904610583087d9af61080b4c5e3152
oisigohan <haruka0526.25@gmail.com>
2026-04-19 22:15:37 +0900

    fix scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import { HelmetProvider } from 'react-helmet-async'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
 import WorkPage from './pages/WorkPage'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
 
 export default function App() {
   return (
     <HelmetProvider>
     <BrowserRouter>
       <div className="min-h-screen flex flex-col bg-white text-neutral-800 font-sans">
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
